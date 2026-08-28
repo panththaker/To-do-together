03f1e9aa69d966f319554466a35c8bd874f25bb8
Panth Thaker <thakep3@mcmaster.ca>
2026-08-28 18:58:53 -0400

    loginscreen file made

---
+++ composeApp/src/commonMain/kotlin/com/jpt/todotogether/auth/presentation/LoginScreen.kt
@@ -0,0 +1,16 @@
+package com.jpt.todotogether.auth.presentation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun LoginScreen(){
+    Column(){
+
+    }
+}
